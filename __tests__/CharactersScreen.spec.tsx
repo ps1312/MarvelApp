2b6511b174f7b26ebff7f6949b4e93bffd9c359d
Paulo Sérgio <ps131296@gmail.com>
2021-06-22 11:05:59 -0300

    CharactersScreen should make a characters request and display a loading indicator on init

--- __tests__/CharactersScreen.spec.tsx
+++ __tests__/CharactersScreen.spec.tsx
@@ -1,15 +1,66 @@
 import React from 'react';
 import {ActivityIndicator} from 'react-native';
 import renderer from 'react-test-renderer';
+import md5 from 'md5';
 
-const CharactersScreen = () => {
+import {Character} from '../src/api';
+
+const md5MockedValue = 'hashed';
+
+jest.mock('md5', () => jest.fn().mockReturnValue(md5MockedValue));
+
+type Props = {
+  listCharactersService: (url: string) => Promise<Character>;
+  timestamp: () => number;
+  publicKey: string;
+  privateKey: string;
+  baseUrl: string;
+};
+
+const CharactersScreen = ({
+  listCharactersService,
+  timestamp,
+  publicKey,
+  privateKey,
+  baseUrl,
+}: Props) => {
+  const hash = md5(timestamp + publicKey + privateKey);
+
+  listCharactersService(
+    baseUrl + `?ts=${timestamp()}&limit=10&apikey=${publicKey}&hash=${hash}`,
+  );
+
   return <ActivityIndicator />;
 };
 
 describe('CharactersScreen.tsx', () => {
-  test('should display a loading indicator on init', () => {
-    const testInstance = renderer.create(<CharactersScreen />).root;
+  test('should make request to characters and display a loading indicator on init', () => {
+    const listCharactersService = jest.fn();
+    const timestamp = () => 9999999999999;
+    const publicKey = 'public marvel key';
+    const privateKey = 'private marvel key';
+    const baseUrl = 'http://any-url.com/';
+
+    const testInstance = renderer.create(
+      <CharactersScreen
+        listCharactersService={listCharactersService}
+        timestamp={timestamp}
+        publicKey={publicKey}
+        privateKey={privateKey}
+        baseUrl={baseUrl}
+      />,
+    ).root;
+
     const activityIndicator = testInstance.findByType(ActivityIndicator);
     expect(activityIndicator).not.toBeNull();
+
+    expect(md5).toHaveBeenCalledTimes(1);
+    expect(md5).toHaveBeenCalledWith(timestamp + publicKey + privateKey);
+
+    const expectedUrl =
+      baseUrl +
+      `?ts=${timestamp()}&limit=10&apikey=${publicKey}&hash=${md5MockedValue}`;
+    expect(listCharactersService).toHaveBeenCalledTimes(1);
+    expect(listCharactersService).toHaveBeenCalledWith(expectedUrl);
   });
 });
