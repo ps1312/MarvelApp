6d07c111409e468bf763a2da54e527ee6d2de1ea
Paulo Sérgio <ps131296@gmail.com>
2021-06-21 23:25:25 -0300

    listHeroesService throws error when fetch returns invalid data

--- __tests__/ListHeroesService.spec.tsx
+++ __tests__/ListHeroesService.spec.tsx
@@ -3,12 +3,13 @@ global.fetch = jest.fn();
 const listHeroesService = async (url: string) => {
   try {
     await fetch(url);
+    throw new Error();
   } catch {
     throw new Error();
   }
 };
 
-describe('', () => {
+describe('listHeroesService()', () => {
   test('make request with provided url', () => {
     const url = 'https://any-url.com';
     listHeroesService(url);
@@ -19,6 +20,13 @@ describe('', () => {
 
   test('throws error when fetch fails', async () => {
     global.fetch = jest.fn().mockRejectedValueOnce('');
+
+    const url = 'https://any-url.com';
+    await expect(listHeroesService(url)).rejects.toThrow();
+  });
+
+  test('throws error when fetch returns invalid data', async () => {
+    global.fetch = jest.fn().mockResolvedValueOnce('');
 
     const url = 'https://any-url.com';
     await expect(listHeroesService(url)).rejects.toThrow();
