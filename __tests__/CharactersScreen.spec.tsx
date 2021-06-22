ba0686d234bfbd39b3e6cf935d440d53c9544c7e
Paulo Sérgio <ps131296@gmail.com>
2021-06-22 11:46:46 -0300

    moved CharactersScreen to its own file, removed limit=10 query param from request

--- __tests__/CharactersScreen.spec.tsx
+++ __tests__/CharactersScreen.spec.tsx
@@ -1,56 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, Button, View} from 'react-native';
+import React from 'react';
 import {waitForElementToBeRemoved, render} from '@testing-library/react-native';
 import md5 from 'md5';
-
-import {Character} from '../src/api';
+import CharactersScreen from '../src/CharactersScreen';
 
 const md5MockedValue = 'hashed';
 jest.mock('md5', () => jest.fn().mockReturnValue(md5MockedValue));
-
-type Props = {
-  listCharactersService: (url: string) => Promise<Character>;
-  timestamp: () => number;
-  publicKey: string;
-  privateKey: string;
-  baseUrl: string;
-};
-
-const CharactersScreen = ({
-  listCharactersService,
-  timestamp,
-  publicKey,
-  privateKey,
-  baseUrl,
-}: Props) => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function makeRequest() {
-      const hash = md5(timestamp() + publicKey + privateKey);
-
-      try {
-        const queryParams = `?ts=${timestamp()}&limit=10&apikey=${publicKey}&hash=${hash}`;
-        const url = baseUrl + queryParams;
-        await listCharactersService(url);
-      } catch {}
-
-      setLoading(false);
-    }
-
-    makeRequest();
-  }, [baseUrl, listCharactersService, privateKey, publicKey, timestamp]);
-
-  return (
-    <View>
-      {loading ? (
-        <ActivityIndicator testID={'activityIndicator'} />
-      ) : (
-        <Button title={'RETRY'} onPress={() => {}} />
-      )}
-    </View>
-  );
-};
 
 describe('CharactersScreen.tsx', () => {
   test('should make request to characters and display a loading indicator on init', async () => {
@@ -75,8 +29,7 @@ describe('CharactersScreen.tsx', () => {
     expect(md5).toHaveBeenCalledWith(timestamp() + publicKey + privateKey);
 
     const expectedUrl =
-      baseUrl +
-      `?ts=${timestamp()}&limit=10&apikey=${publicKey}&hash=${md5MockedValue}`;
+      baseUrl + `?ts=${timestamp()}&apikey=${publicKey}&hash=${md5MockedValue}`;
     expect(listCharactersService).toHaveBeenCalledTimes(1);
     expect(listCharactersService).toHaveBeenCalledWith(expectedUrl);
 
---
+++ src/CharactersScreen.tsx
@@ -0,0 +1,50 @@
+import md5 from 'md5';
+import React, {useState, useEffect} from 'react';
+import {View, ActivityIndicator, Button} from 'react-native';
+import {Character} from './api';
+
+type Props = {
+  listCharactersService: (url: string) => Promise<Character[]>;
+  timestamp: () => number;
+  publicKey: string;
+  privateKey: string;
+  baseUrl: string;
+};
+
+const CharactersScreen = ({
+  listCharactersService,
+  timestamp,
+  publicKey,
+  privateKey,
+  baseUrl,
+}: Props) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function makeRequest() {
+      const hash = md5(timestamp() + publicKey + privateKey);
+
+      try {
+        const queryParams = `?ts=${timestamp()}&apikey=${publicKey}&hash=${hash}`;
+        const url = baseUrl + queryParams;
+        await listCharactersService(url);
+      } catch {}
+
+      setLoading(false);
+    }
+
+    makeRequest();
+  }, [baseUrl, listCharactersService, privateKey, publicKey, timestamp]);
+
+  return (
+    <View>
+      {loading ? (
+        <ActivityIndicator testID={'activityIndicator'} />
+      ) : (
+        <Button title={'RETRY'} onPress={() => {}} />
+      )}
+    </View>
+  );
+};
+
+export default CharactersScreen;
