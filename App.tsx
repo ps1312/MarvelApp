778e60f793847a0e42ecec43b71b0c47aa6f246b
Paulo Sérgio <ps131296@gmail.com>
2021-06-22 10:24:25 -0300

    add characters request to app.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import md5 from 'md5';
+import listHeroesService from './src/api';
+
+const PUBLIC_KEY = 'd38c3e71397286b3b21d3ea99160fb8b';
+const PRIVATE_KEY = '7ec5298d4994c38856f9e1427e9fd8ccd1555137';
 
 const App = () => {
+  useEffect(() => {
+    async function makeRequest() {
+      const timestamp = Number(new Date());
+      const hash = md5(timestamp + PRIVATE_KEY + PUBLIC_KEY);
+      const result = await listHeroesService(
+        `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&limit=10&apikey=${PUBLIC_KEY}&hash=${hash}`,
+      );
+
+      console.log(result);
+    }
+
+    makeRequest();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Marvel App</Text>
@@ -19,3 +38,5 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+// ["id", "name", "description", "modified", "thumbnail", "resourceURI", "comics", "series", "stories", "events", "urls"]
