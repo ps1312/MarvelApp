62a9213e6af9aa0da676f0c0a42ee41bfb2e61e0
Paulo Sérgio <ps131296@gmail.com>
2021-06-23 10:19:00 -0300

    add more cases to Pagination number of pages display test

--- __tests__/Pagination.spec.tsx
+++ __tests__/Pagination.spec.tsx
@@ -6,13 +6,22 @@ interface Props {
   total: number;
 }
 const Pagination = ({total}: Props) => {
-  return <Text>{total / 10}</Text>;
+  const totalPages = Math.ceil(total / 10);
+  return <Text>{totalPages}</Text>;
 };
 
 describe('Pagination.tsx', () => {
-  test('displays one page when total is 10', () => {
-    const {getByText} = render(<Pagination total={10} />);
+  test('displays number of pages', () => {
+    const {getByText, rerender} = render(<Pagination total={10} />);
+    expect(getByText('1')).not.toBeNull();
 
+    rerender(<Pagination total={20} />);
+    expect(getByText('2')).not.toBeNull();
+
+    rerender(<Pagination total={14} />);
+    expect(getByText('2')).not.toBeNull();
+
+    rerender(<Pagination total={5} />);
     expect(getByText('1')).not.toBeNull();
   });
 });
