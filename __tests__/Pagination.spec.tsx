61dfb40c8ce2a3373f4227d374684d8e7971a920
Paulo Sérgio <ps131296@gmail.com>
2021-06-23 10:40:18 -0300

    ensure prev and next buttons are disabled correctly

--- __tests__/Pagination.spec.tsx
+++ __tests__/Pagination.spec.tsx
@@ -1,44 +1,87 @@
 import {render} from '@testing-library/react-native';
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, Button} from 'react-native';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 interface Props {
+  current: number;
   total: number;
 }
-const Pagination = ({total}: Props) => {
+const Pagination = ({total, current}: Props) => {
   const totalPages = Math.ceil(total / 10);
   return (
     <View>
-      <TouchableOpacity>
-        <Text>◀</Text>
-      </TouchableOpacity>
+      <Button
+        title={'◀'}
+        accessibilityLabel={'arrow-left'}
+        disabled={current === 1}
+        onPress={() => {}}
+      />
       <Text>{totalPages}</Text>
-      <TouchableOpacity>
-        <Text>▶</Text>
-      </TouchableOpacity>
+      <Button
+        title={'▶'}
+        accessibilityLabel={'arrow-right'}
+        disabled={current === totalPages}
+        onPress={() => {}}
+      />
     </View>
   );
 };
 
 describe('Pagination.tsx', () => {
   test('displays number of pages', () => {
-    const {getByText, rerender} = render(<Pagination total={10} />);
+    const {getByText, rerender} = render(<Pagination current={1} total={10} />);
     expect(getByText('1')).not.toBeNull();
 
-    rerender(<Pagination total={20} />);
+    rerender(<Pagination current={1} total={20} />);
     expect(getByText('2')).not.toBeNull();
 
-    rerender(<Pagination total={14} />);
+    rerender(<Pagination current={1} total={14} />);
     expect(getByText('2')).not.toBeNull();
 
-    rerender(<Pagination total={5} />);
+    rerender(<Pagination current={1} total={5} />);
     expect(getByText('1')).not.toBeNull();
   });
 
   test('displays prev and next page buttons', () => {
-    const {getByText} = render(<Pagination total={10} />);
+    const {getByText} = render(<Pagination current={1} total={10} />);
 
     expect(getByText('◀')).not.toBeNull();
     expect(getByText('▶')).not.toBeNull();
+  });
+
+  test('prev and next buttons are disabled correctly', () => {
+    const {getByLabelText, rerender} = render(
+      <Pagination current={1} total={10} />,
+    );
+
+    expect(
+      getByLabelText('arrow-left').props.accessibilityState.disabled,
+    ).toBeTruthy();
+
+    expect(
+      getByLabelText('arrow-right').props.accessibilityState.disabled,
+    ).toBeTruthy();
+
+    rerender(<Pagination current={2} total={30} />);
+
+    expect(
+      getByLabelText('arrow-left').props.accessibilityState.disabled,
+    ).toBeFalsy();
+
+    expect(
+      getByLabelText('arrow-right').props.accessibilityState.disabled,
+    ).toBeFalsy();
+
+    rerender(<Pagination current={3} total={30} />);
+
+    expect(
+      getByLabelText('arrow-left').props.accessibilityState.disabled,
+    ).toBeFalsy();
+
+    expect(
+      getByLabelText('arrow-right').props.accessibilityState.disabled,
+    ).toBeTruthy();
   });
 });
