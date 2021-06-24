c98c8819fdd23b5a7f4595d2f3a828aade702a83
Paulo Sérgio <ps131296@gmail.com>
2021-06-23 21:21:55 -0300

    append timestamp, public key and hash to url as query params

--- __tests__/FetchDecorator.spec.tsx
+++ __tests__/FetchDecorator.spec.tsx
@@ -1,18 +1,17 @@
 import md5 from 'md5';
+import {PUBLIC_KEY, PRIVATE_KEY} from '@env';
 
 const md5MockedValue = 'hashed';
 jest.mock('md5', () => jest.fn().mockReturnValue(md5MockedValue));
 
-const PRIVATE_KEY = '7ec5298d4994c38856f9e1427e9fd8ccd1555137';
-const PUBLIC_KEY = 'd38c3e71397286b3b21d3ea99160fb8b';
-
 const decorateUrl = (url: string) => {
   const timestamp = Date.now();
 
-  md5(timestamp + PRIVATE_KEY + PUBLIC_KEY);
+  const hash = md5(timestamp + PRIVATE_KEY + PUBLIC_KEY);
 
   const decoratedUrl = url + 'ts';
-  return decoratedUrl;
+  let queryParams = `?&ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`;
+  return decoratedUrl + queryParams;
 };
 
 describe('decorateUrl()', () => {
@@ -24,6 +23,16 @@ describe('decorateUrl()', () => {
 
     expect(md5).toHaveBeenCalledTimes(1);
     expect(md5).toHaveBeenCalledWith(timestamp + PRIVATE_KEY + PUBLIC_KEY);
+  });
+
+  test('append timestamp, public key and hash to url as query params', () => {
+    const timestamp = 1111111111111;
+    jest.spyOn(Date, 'now').mockReturnValue(timestamp);
+    const newUrl = decorateUrl('http://any-url.com');
+
+    expect(newUrl.includes(`ts=${timestamp}`)).toBeTruthy();
+    expect(newUrl.includes(`apikey=${PUBLIC_KEY}`)).toBeTruthy();
+    expect(newUrl.includes(`hash=${md5MockedValue}`)).toBeTruthy();
   });
 });
 
