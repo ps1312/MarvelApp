e9217fea4f194ce6f23aec800501737b2bb509e9
Paulo Sérgio <ps131296@gmail.com>
2021-06-24 10:52:51 -0300

    add different renders for every item type in character details

--- src/components/CharacterDetailsScreen.tsx
+++ src/components/CharacterDetailsScreen.tsx
@@ -8,10 +8,12 @@ import {
   StyleSheet,
   TouchableOpacity,
   SafeAreaView,
+  View,
 } from 'react-native';
 import {CharacterEvent} from '../services/getCharactersEvents';
 import {Serie} from '../services/getCharactersSeries';
 import {Character} from '../services/listCharactersService';
+import theme from '../theme';
 
 const CharacterDetailsScreen = ({
   getCharacterSeries,
@@ -61,10 +63,56 @@ const CharacterDetailsScreen = ({
     },
   ];
 
+  const renderCharacter = (item: Character | undefined) => {
+    return (
+      <View style={styles.characterInfoContainer}>
+        <Image
+          style={styles.characterImage}
+          source={{uri: item?.thumbUrl}}
+          accessibilityLabel={item?.thumbUrl}
+        />
+      </View>
+    );
+  };
+
+  const renderSerie = (item: Serie) => {
+    return (
+      <>
+        <Image
+          style={styles.thumbnailImage}
+          source={{uri: item.thumbUrl}}
+          accessibilityLabel={item.thumbUrl}
+        />
+        <Text>{item.title}</Text>
+
+        <Text>{item.description}</Text>
+        <Text>{item.startYear}</Text>
+        <Text>{item.startYear}</Text>
+      </>
+    );
+  };
+
+  const renderEvent = (item: CharacterEvent) => {
+    return (
+      <>
+        <Image
+          style={styles.thumbnailImage}
+          source={{uri: item.thumbUrl}}
+          accessibilityLabel={item.thumbUrl}
+        />
+        <Text>{item.title}</Text>
+
+        <Text>{item.description}</Text>
+        <Text>{item.start}</Text>
+        <Text>{item.end}</Text>
+      </>
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity onPress={onDetailsClose}>
-        <Text>Voltar</Text>
+      <TouchableOpacity style={styles.headerContainer} onPress={onDetailsClose}>
+        <Text style={styles.headerCloseText}>X</Text>
       </TouchableOpacity>
       {error ? (
         <Button title={'Tentar novamente'} onPress={() => fetchDetails()} />
@@ -79,7 +127,22 @@ const CharacterDetailsScreen = ({
           <SectionList<any>
             sections={DATA}
             keyExtractor={(item, index) => item.title + index}
-            renderSectionHeader={({section: {title}}) => <Text>{title}</Text>}
+            renderSectionHeader={({section: {title}}) => {
+              if (title === character?.name) {
+                return (
+                  <View style={styles.characterInfoSectionHeaderContainer}>
+                    <Text style={styles.characterInfoSectionHeaderText}>
+                      {title}
+                    </Text>
+                  </View>
+                );
+              }
+              return (
+                <View style={styles.sectionHeaderContainer}>
+                  <Text style={styles.sectionHeaderContainerText}>{title}</Text>
+                </View>
+              );
+            }}
             renderSectionFooter={({section}: any) => {
               if (section.data.length === 0) {
                 return <Text>{section.emptyState}</Text>;
@@ -87,17 +150,13 @@ const CharacterDetailsScreen = ({
 
               return null;
             }}
-            renderItem={({item}) => {
-              return (
-                <>
-                  <Image
-                    style={styles.characterImage}
-                    source={{uri: item.thumbUrl}}
-                    accessibilityLabel={item.thumbUrl}
-                  />
-                  <Text>{item.title}</Text>
-                </>
-              );
+            renderItem={({item, section}) => {
+              if (section.title === character?.name) {
+                return renderCharacter(character);
+              } else if (section.title === 'Series') {
+                return renderSerie(item);
+              }
+              return renderEvent(item);
             }}
           />
         </>
@@ -119,9 +178,54 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  headerContainer: {
+    height: 44,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  headerCloseText: {
+    fontSize: 28,
+    alignSelf: 'center',
+    marginRight: 24,
+  },
   characterImage: {
-    width: 40,
-    height: 40,
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    borderWidth: 2,
+    borderColor: theme.primaryColor,
+  },
+  characterInfoContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingVertical: 24,
+  },
+  characterInfoSectionHeaderContainer: {
+    height: 44,
+    backgroundColor: 'white',
+  },
+  characterInfoSectionHeaderText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  thumbnailImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  sectionHeaderContainer: {
+    height: 44,
+    backgroundColor: theme.primaryColor,
+    justifyContent: 'center',
+    paddingHorizontal: 14,
+    marginBottom: 16,
+  },
+  sectionHeaderContainerText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
